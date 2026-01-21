6281679d6fbc3ae0e8162e0dec562b2a6da33b1f
Mohammed Dadi <artofmohammed@gmail.com>
2026-01-21 23:33:16 +0100

    testing basic features such as loops

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+   public static void main(String[] args) {
+      int x = 1;
+      while(x <= 2){
+         System.out.println("Doo");
+         System.out.println("Bee");
+
+         x = x + 1;
+      }
+      if (x == 3){
+         System.out.println("Do");
+      }
+
+   }
+}
